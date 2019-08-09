a64ba76a75463228f5d645170ccb568996484c64
alcbotta <alcbotta@gmail.com>
2019-08-09 08:20:48 +0000

    commit 313e5b63f289e138492795f3c4998102390aae65 Author: Andre Botta <alcbotta@gmail.com> Date:   Fri Aug 9 08:19:16 2019 +0000
        test

---
+++ precache-manifest.cd57be0b215e67a590eee4b1a051849b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103561d73e65fb3c52d5efe33539d593",
+    "url": "/drone_react/index.html"
+  },
+  {
+    "revision": "c1451d62ffe1d9d4f3c9",
+    "url": "/drone_react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/drone_react/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "c1451d62ffe1d9d4f3c9",
+    "url": "/drone_react/static/js/main.f0e10b1d.chunk.js"
+  },
+  {
+    "revision": "1a6c4e7086cadb5221f5",
+    "url": "/drone_react/static/js/runtime~main.008677a6.js"
+  }
+]);
